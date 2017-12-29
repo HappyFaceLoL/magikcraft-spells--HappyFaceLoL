4a6621f1a9d2e0d7888f84ae7ef9c0f231861a20
HappyFaceLoL <babatish@hotmail.com>
2017-12-29 03:21:47 +0000

    Update pot.ts

--- pot.ts
+++ pot.ts
@@ -4,6 +4,6 @@ function pot() {
     const ingredients = []
     ingredients.push("PUT_INGREDIENT_HERE")
     ingredients.push("PUT_ANOTHER_INGREDIENT_HERE")
-    ingredients.push("PUT_A_THIRD_INGREDIENT_HERE)
+    ingredients.push("PUT_A_THIRD_INGREDIENT_HERE")
     magik.caldarium(ingredients)
 }
