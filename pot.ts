89aea491a2f995862f9228eb3b8551359c732d8c
HappyFaceLoL <babatish@hotmail.com>
2017-12-29 03:17:52 +0000

    Update pot.ts

---
+++ pot.ts
@@ -0,0 +1,9 @@
+const magik = magikcraft.io
+
+function pot() {
+    const ingredients = []
+    ingredients.push("PUT_INGREDIENT_HERE")
+    ingredients.push("PUT_ANOTHER_INGREDIENT_HERE")
+    ingredients.push("PUT_A_THIRD_INGREDIENT_HERE)
+    magik.caldarium(ingredients)
+}
